31c85e1643d7d5eeac3b2cc08ae8fb269ae44862
diegosalas27 <dominicsc2hs@gmail.com>
2018-09-22 21:04:29 -0500

    my solution

--- Solution Diego/ShortestPathMain/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse('<h1>ShortestPath app</h1>')
---
+++ Solution Diego/parcial.py
@@ -0,0 +1,95 @@
+import csv
+
+def calcularDistancia(longitud1, longitud2, latitud1, latitud2):
+    return ((longitud1-longitud2)**2 + (latitud1-latitud2)**2)**(0.5)
+
+def dijkstra(dicEnlaces, dicPesos, dicAsignacionID, start, goal):
+    
+    shortest_distance = {}
+    predecessor = {}
+    unseenNodes = dicEnlaces
+    distanciaNodos = dicPesos
+    idNodos = dicAsignacionID
+    infinity = 999999
+    path = []
+    
+    for node in distanciaNodos:
+        shortest_distance[node] = infinity
+    shortest_distance[start] = 0
+    
+    while unseenNodes:
+        minNode = None
+        for node in unseenNodes:
+            if minNode is None:
+                minNode = node
+            elif shortest_distance[node] < shortest_distance[minNode]:
+                minNode = node
+        
+        if distanciaNodos[minNode] + shortest_distance[minNode] < shortest_distance[unseenNodes[minNode]]:
+            shortest_distance[unseenNodes[minNode]] = distanciaNodos[minNode] + shortest_distance[minNode] 
+            predecessor[unseenNodes[minNode]] = minNode
+        unseenNodes.pop(minNode)
+    
+    print(shortest_distance[goal])
+    
+#     currentNode = goal
+#     while currentNode != start:
+#         try:
+#             path.insert(0, currentNode)
+#             currentNode = predecessor[currentNode]
+#         except KeyError:
+#             print('No existe camino')
+#             break
+            
+#     print(shortest_distance[goal])
+ 
+            
+#     if shortest_distance[goal] != infinity:
+#         print('La distancia más corta es:', shortest_distance[goal])
+#         print('El camino es es:', path)
+
+def readCSV(filename):
+    """
+    Function that recieves a filename, reads that csv file and returns a dictionary with 
+    the name, xcord and ycord of each populated center of the file. 
+    """
+    dictionary = {}
+    with open(filename) as csvfile:
+        file = csv.reader(csvfile, delimiter=',')
+        idelem = -1
+        for row in file:
+            if idelem != -1:
+                elem = {}
+                elem["id"] = row[0]
+                elem["nombre"] = str(row[1])
+                elem["idForaneo"] = int(row[2])
+                elem["longitud"] = float(row[3])
+                elem["latitud"] = float(row[4])
+                dictionary[idelem] = elem
+                del elem
+            
+            idelem += 1
+    
+    return dictionary
+
+def completaDiccionarios(filename, start, goal):
+    
+    dictionary = readCSV(filename)
+#     print(dictionary)
+    dicPesos = {}
+    dicEnlaces = {}
+    dicAsignacionID = {}
+
+    for key in dictionary:
+        dicPesos[key] = calcularDistancia(dictionary[key]['longitud'],dictionary[dictionary[key]['idForaneo']]['longitud'],
+                                         dictionary[key]['latitud'], dictionary[dictionary[key]['idForaneo']]['latitud'])
+        dicEnlaces[key] = dictionary[key]['idForaneo']
+        dicAsignacionID[key] = dictionary[key]['nombre']
+        
+#     print('pesos: ', dicPesos)
+#     print('enlaces: ', dicEnlaces)
+#     print('asignacionID: ', dicAsignacionID)
+
+    dijkstra(dicEnlaces, dicPesos, dicAsignacionID, start, goal)
+
+completaDiccionarios('prueba.csv', 0, 1)
